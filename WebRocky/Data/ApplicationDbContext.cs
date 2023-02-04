e79f25adf3d9bc0e836da318ad8c5848b91bbec0
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-02-04 10:46:20 +0100

    db setup

---
+++ WebRocky/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebRocky.Models;
+
+namespace WebRocky.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
