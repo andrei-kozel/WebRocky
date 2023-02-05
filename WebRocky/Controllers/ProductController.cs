0e93a2b29e921e98fa1923e3c730707909306d2d
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-02-05 12:42:56 +0100

    Create ProductController.cs

---
+++ WebRocky/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using WebRocky.Data;
+using WebRocky.Models;
+
+namespace WebRocky.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objectList = _db.Product;
+
+            foreach (var prod in objectList)
+            {
+                prod.Category = _db.Category.FirstOrDefault(u => u.Id == prod.CategoryId);
+            }
+
+            return View(objectList);
+        }
+
+    }
+}
