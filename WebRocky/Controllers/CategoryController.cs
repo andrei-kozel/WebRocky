830b1f78fc5f6b2b2170df0a678b2d0992529be2
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-02-05 10:01:59 +0100

    add validation

--- WebRocky/Controllers/CategoryController.cs
+++ WebRocky/Controllers/CategoryController.cs
@@ -30,9 +30,13 @@ namespace WebRocky.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
--- WebRocky/Models/Category.cs
+++ WebRocky/Models/Category.cs
@@ -7,8 +7,12 @@ namespace WebRocky.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Display Order for category must be greater than 0")]
         [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
