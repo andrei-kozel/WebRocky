70799c23eb986ce8a2312e33101bc8a746c021e7
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-02-05 10:35:24 +0100

    add delete functionality

--- WebRocky/Controllers/CategoryController.cs
+++ WebRocky/Controllers/CategoryController.cs
@@ -67,5 +67,36 @@ namespace WebRocky.Controllers
             }
             return View();
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
