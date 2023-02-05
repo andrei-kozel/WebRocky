063553f43fb0cd979a5a4b8ebcef2ccfeff2ffa7
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-02-05 10:21:08 +0100

    add edit functionality

--- WebRocky/Controllers/CategoryController.cs
+++ WebRocky/Controllers/CategoryController.cs
@@ -38,5 +38,34 @@ namespace WebRocky.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
