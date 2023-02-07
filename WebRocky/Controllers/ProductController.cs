fecb25762d66461f653a07d61d0dfbd928a8b1a7
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-02-07 21:38:16 +0100

    upload images

--- WebRocky/Controllers/ProductController.cs
+++ WebRocky/Controllers/ProductController.cs
@@ -1,20 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Collections;
+using WebRocky.Models.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using WebRocky.Data;
 using WebRocky.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
 
 namespace WebRocky.Controllers
 {
     public class ProductController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public ProductController(ApplicationDbContext db)
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
         {
             _db = db;
+            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
@@ -31,28 +36,63 @@ namespace WebRocky.Controllers
 
         public IActionResult Upsert(int? id)
         {
-            IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(i => new SelectListItem
+            ProductVM productVM = new ProductVM()
             {
-                Text = i.Name,
-                Value = i.Id.ToString(),
-            });
-            Product product = new Product();
-
-            ViewBag.CategoryDropDown = CategoryDropDown;
+                Product = new Product(),
+                CategorySelectList = _db.Category.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString(),
+                })
+            };
 
             if (id == null)
             {
-                return View(product);
+                return View(productVM);
             }
             else
             {
-                product = _db.Product.Find(id);
-                if (product == null)
+                productVM.Product = _db.Product.Find(id);
+                if (productVM.Product == null)
                 {
                     return NotFound();
                 }
-                return View(product);
+                return View(productVM);
             }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                string rootPath = _hostEnvironment.WebRootPath;
+
+                if (productVM.Product.Id == 0)
+                {
+                    string upload = rootPath + WC.ImagePath;
+                    string fileName = Guid.NewGuid().ToString();
+                    string extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStream = new FileStream(Path.Combine(upload, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+
+                    productVM.Product.Image = fileName + extension;
+                    _db.Product.Add(productVM.Product);
+
+                }
+                else
+                {
+                    // update product
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
 
     }
